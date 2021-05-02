7c33950fd0a48e05fa14ad98803da737bf1b8a58
[Carlos] <[carloslv10@gmail.com]>
2021-05-02 15:29:05 -0600

    Primer commit

---
+++ webpack.prod.js
@@ -0,0 +1,85 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MinicssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+
+
+
+
+module.exports = {
+
+    mode: 'production',
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+      },
+    output: {
+        filename: 'main.[contenthash].js',
+        clean: true, // no me genera otro main si hice cambios
+    },
+    module: {
+        rules: [{
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MinicssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    minimize: false,
+                    sources: false,
+                },
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        esModule: false,
+                        //name: 'assets/[name].[ext]'
+                    }
+                }]
+            },
+        ]
+    },
+    
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MinicssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [{
+                from: 'src/assets',
+                to: 'assets/'
+            }]
+        }),
+
+    ]
+
+}
